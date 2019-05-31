e0eefdb3203e76bf710615834f63bd2c037a695b
D. Scott Kesler <skesler@ebsco.com>
2019-05-31 13:05:33 -0500

    add build folder

---
+++ build/precache-manifest.05a4e9c3c277c6969a9cf0aabd168d50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54fde6bc9334b7657316c4a12d72f65c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97be935a0c4a9b6149e4",
+    "url": "/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "97be935a0c4a9b6149e4",
+    "url": "/static/js/main.bdc278aa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
